2a0476cd878991c811183ec683b617274dec1cc5
Ramox32 <60747330+Ramox32@users.noreply.github.com>
2025-04-03 19:12:13 +0200

    Fix Example 1

--- Example_1.ino
+++ Example_1.ino
@@ -1,24 +1,17 @@
 #define LED_PIN 4
-#define BUTTON 10 
- 
 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int buttonPin = digitalRead(BUTTON);
-
-  if(buttonPin == LOW){
-    digitalWrite(LED_PIN, LOW);
-  } else{
-    digitalWrite(LED_PIN, HIGH);
-  }
-  
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
   
 
 }
